9cd559fd98204c61ad2ee4887366df753e79d25a
sarahyack <143653575+sarahyack@users.noreply.github.com>
2023-09-15 12:56:16 -0600

    Update config.py

--- MacawProj/src/helper/config.py
+++ MacawProj/src/helper/config.py
@@ -1,6 +1,13 @@
 #Change this for file paths.
+#Path to model
 model_path = "C:\\Users\\Sarah\\Documents\\My Projects\\Coding\\VS PyProjects\\MacawProj\\my_model"
+#Path to image data (training data)
 image_data_path = "C:\\Users\\Sarah\\Documents\\My Projects\\Coding\\VS PyProjects\\MacawProj\\image-data\\image_data.pkl"
-folder1_path = "C:\\Users\\Sarah\\Documents\\My Projects\\Coding\\ML Testing\\MLFA Project\\Training\\Not Birds"
-folder2_path = "C:\\Users\\Sarah\\Documents\\My Projects\\Coding\\ML Testing\\MLFA Project\\Training\\Birds"
-folder3_path = "C:\\Users\\Sarah\\Documents\\My Projects\\Coding\\ML Testing\\MLFA Project\\Training\\Macaws"
+#Paths for processing training data
+folder1_path = "C:\\Users\\Sarah\\Documents\\My Projects\\Coding\\Machine Learning\\MLFA Project\\Training\\Not Birds"
+folder2_path = "C:\\Users\\Sarah\\Documents\\My Projects\\Coding\\Machine Learning\\MLFA Project\\Training\\Birds"
+folder3_path = "C:\\Users\\Sarah\\Documents\\My Projects\\Coding\\Machine Learning\\MLFA Project\\Training\\Macaws"
+#Paths for processing test data
+folder4_path = "C:\\Users\\Sarah\\Documents\\My Projects\\Coding\\Machine Learning\\MLFA Project\\Testing\\Not Birds"
+folder5_path = "C:\\Users\\Sarah\\Documents\\My Projects\\Coding\\Machine Learning\\MLFA Project\\Testing\\Birds"
+folder6_path = "C:\\Users\\Sarah\\Documents\\My Projects\\Coding\\Machine Learning\\MLFA Project\\Testing\\Macaws"
